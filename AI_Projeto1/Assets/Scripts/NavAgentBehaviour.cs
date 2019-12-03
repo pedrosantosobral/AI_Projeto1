50022db3266f67b3c605e00c422675125dfb6273
Joana Marques <joana.pipaa@gmail.com>
2019-12-03 17:13:58 +0000

    Add first state and fail miserably

--- AI_Projeto1/Assets/Scripts/NavAgentBehaviour.cs
+++ AI_Projeto1/Assets/Scripts/NavAgentBehaviour.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using URandom = UnityEngine.Random;
 using UnityEngine;
 using UnityEngine.AI;
 using LibGameAI.FSMs;
@@ -39,82 +41,123 @@ public class NavAgentBehaviour : MonoBehaviour
     // Reference to the NavMeshAgent component
     private NavMeshAgent _agent;
 
+    //Reference to state machine
+    private StateMachine stateMachine;
+
     private void Start()
     {
-        //generate stage preferences values
-        _stage1TotalTime = Random.Range(30f, 70f);
-        _stage2TotalTime = 100f - _stage1TotalTime;
-        _stage1Time      = _stage1TotalTime;
-        _stage2Time      = _stage2TotalTime; 
+        GenerateAIAgentStats();
+        CreateFSM();
 
-        //generate random health values
-        _maxHealth          = Random.Range(80f, 100f);
-        _startingHealth     = Random.Range(0, _maxHealth);
-        _healthLoseSpeed    = Random.Range(0.1f, 0.5f);
-        _healthGainSpeed    = Random.Range(3f, 5f);
-        _health             = _startingHealth;
-
-        //generate random stamina values
-        _maxStamina         = Random.Range(70f, 100f);
-        _startingStamina    = Random.Range(0, _maxStamina);
-        _staminaLoseSpeed   = Random.Range(0.5f, 1f);
-        _staminaGainSpeed   = Random.Range(3f, 5f);
-        _stamina            = _startingStamina;
-
-        //Get references
-        _stage1      = GameObject.Find("Stage1");
-        _stage2      = GameObject.Find("Stage2");
-        _chillZone1  = GameObject.Find("ChillZone1");
-        _chillZone2  = GameObject.Find("ChillZone2");
-        _food        = GameObject.Find("Food");
-    
         // Get reference to the NavMeshAgent component
         _agent = GetComponent<NavMeshAgent>();
-        // Set initial agent goal
-        //_agent.destination = _stage1 .transform.position;
     }
 
-    // Method called when agent collides with something
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        // Did agent collide with goal?
-        if (other.name == "Goal")
-            // If so, update destination (let goal reposition itself first)
-            Invoke("UpdateDestination", 0.1f);
+        Action actions = stateMachine.Update();
+        actions?.Invoke();
     }
 
-    // Update destination
-    private void UpdateDestination()
-    {
-        // Set destination to current goal position
-        _agent.destination = _stage1.transform.position;
-    }
 
     private void DecreaseStamina()
     {
         _stamina -= _staminaLoseSpeed * Time.fixedDeltaTime;
+        if (_stamina <= 0)
+        {
+            _stamina = 0;
+        }
     }
 
-    
+
     private void DecreaseHealth()
     {
         _health -= _healthLoseSpeed * Time.fixedDeltaTime;
+        if (_health <= 0)
+        {
+            _health = 0;
+        }
     }
 
     private void IncreaseStamina()
     {
         _stamina += _staminaGainSpeed * Time.fixedDeltaTime;
+        if (_stamina >= _maxStamina)
+        {
+            _stamina = _maxStamina;
+        }
     }
 
     private void IncreaseHealth()
     {
         _health += _healthGainSpeed * Time.fixedDeltaTime;
+        if(_health >= _maxHealth)
+        {
+            _health = _maxHealth;
+        }
     }
 
-    //test update
-    private void FixedUpdate()
+    private void GenerateAIAgentStats()
     {
-        DecreaseStamina();
+        //generate stage preferences values
+        _stage1TotalTime = URandom.Range(30f, 70f);
+        _stage2TotalTime = 100f - _stage1TotalTime;
+        _stage1Time = _stage1TotalTime;
+        _stage2Time = _stage2TotalTime;
+
+        //generate random health values
+        _maxHealth = URandom.Range(80f, 100f);
+        _startingHealth = URandom.Range(0, _maxHealth);
+        _healthLoseSpeed = URandom.Range(0.1f, 0.5f);
+        _healthGainSpeed = URandom.Range(3f, 5f);
+        _health = _startingHealth;
+
+        //generate random stamina values
+        _maxStamina = URandom.Range(70f, 100f);
+        _startingStamina = URandom.Range(0, _maxStamina);
+        _staminaLoseSpeed = URandom.Range(0.5f, 1f);
+        _staminaGainSpeed = URandom.Range(3f, 5f);
+        _stamina = _startingStamina;
+
+        //Get references
+        _stage1 = GameObject.Find("Stage1");
+        _stage2 = GameObject.Find("Stage2");
+        _chillZone1 = GameObject.Find("ChillZone1");
+        _chillZone2 = GameObject.Find("ChillZone2");
+        _food = GameObject.Find("Food");
     }
 
+    private void CreateFSM()
+    {
+        State eatingState = new State("Hungry",
+            () => Debug.Log("Entrou health++"),
+            IncreaseHealth,
+            () => Debug.Log("Saiu"));
+
+        State goEat = new State("Go Eat",
+            ()=> Debug.Log("Entrou"),
+            MoveToFood,
+            null);
+
+        State tiredState = new State("Tired",
+            null,
+            IncreaseStamina,
+            null);
+
+        
+        goEat.AddTransition(
+            new Transition(
+                () => gameObject.GetComponent<NavMeshAgent>().remainingDistance <= gameObject.GetComponent<NavMeshAgent>().stoppingDistance,
+                null,
+                eatingState));
+
+        stateMachine = new StateMachine(goEat);
+    }
+
+    private void MoveToFood()
+    {
+        _agent.destination = _food.transform.position;
+    }
+
+    
 }
