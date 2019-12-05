91d10147095d997eaaf3c9cfc33f2d8deec6d692
Pedro Santos <pedrosantosobral@users.noreply.github.com>
2019-12-05 15:09:58 +0000

    Finish initial state machine and respective methods

--- AI_Projeto1/Assets/Scripts/NavAgentBehaviour.cs
+++ AI_Projeto1/Assets/Scripts/NavAgentBehaviour.cs
@@ -8,15 +8,18 @@ using LibGameAI.FSMs;
 
 public class NavAgentBehaviour : MonoBehaviour
 {
-    private bool        _arrived;
+
+    State initialState;
+
+    private bool        _arrive;
 
     //stage timers variables
 
-    private int         _stage;
-    private int         _startingStage;
     private float       _stage1TotalTime;
+    private float       _stage1LoseTime;
     private float       _stage1Time;
     private float       _stage2TotalTime;
+    private float       _stage2LoseTime;
     private float       _stage2Time;
 
     //Health variables
@@ -63,9 +66,21 @@ public class NavAgentBehaviour : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Goal"))
+        if(other.CompareTag("Food"))
         {
-            _arrived = true;
+            _arrive = true;
+        }
+        if (other.CompareTag("Rest"))
+        {
+            _arrive = true;
+        }
+        if (other.CompareTag("Stage1"))
+        {
+            _arrive = true;
+        }
+        if (other.CompareTag("Stage2"))
+        {
+            _arrive = true;
         }
     }
 
@@ -96,7 +111,7 @@ public class NavAgentBehaviour : MonoBehaviour
         {
             _stamina = _maxStamina;
         }
-        _arrived = false;
+        _arrive = false;
     }
 
     private void IncreaseHealth()
@@ -106,7 +121,7 @@ public class NavAgentBehaviour : MonoBehaviour
         {
             _health = _maxHealth;
         }
-        _arrived = false;
+        _arrive = false;
     }
 
     private void RestingActions()
@@ -155,6 +170,34 @@ public class NavAgentBehaviour : MonoBehaviour
 
     }
 
+    private void WatchingStage1Actions()
+    {
+        _arrive = false;
+        DecreaseHealth();
+        DecreaseStamina();
+        //Reset Stage 2 Watching time
+        _stage2Time = _stage2TotalTime;
+        _stage1Time -= _stage1LoseTime * Time.deltaTime;
+        if (_stage1Time <= 0)
+        {
+            _stage1Time = 0;
+        }
+    }
+
+    private void WatchingStage2Actions()
+    {
+        _arrive = false;
+        DecreaseHealth();
+        DecreaseStamina();
+        //Reset Stage 2 Watching time
+        _stage1Time = _stage1TotalTime;
+        _stage2Time -= _stage1LoseTime * Time.deltaTime;
+        if(_stage2Time <= 0)
+        {
+            _stage2Time = 0;
+        }
+    }
+
     private void GenerateAIAgentStats()
     {
         //generate stage preferences values
@@ -162,6 +205,8 @@ public class NavAgentBehaviour : MonoBehaviour
         _stage2TotalTime = 100f - _stage1TotalTime;
         _stage1Time = _stage1TotalTime;
         _stage2Time = _stage2TotalTime;
+        _stage1LoseTime = URandom.Range(0.1f, 1f);
+        _stage2LoseTime = URandom.Range(0.1f, 1f);
 
         //generate random health values
         _maxHealth = URandom.Range(80f, 100f);
@@ -188,50 +233,55 @@ public class NavAgentBehaviour : MonoBehaviour
     private void CreateFSM()
     {
         State eatingState = new State("Hungry",
-            () => Debug.Log("Entrou health++"),
+            () => Debug.Log("está a comer"),
             EatingActions,
-            () => Debug.Log("Saiu"));
+            () => Debug.Log("parou de comer"));
 
         State goEat = new State("Go Eat",
-            ()=> Debug.Log("Entrou"),
+            ()=> Debug.Log("vai comer"),
             GoEatActions,
-            null);
+            () => Debug.Log("chegou a comida"));
 
         State restingState = new State("Tired",
-            null,
+            () => Debug.Log("está a descansar"),
             RestingActions,
-            null);
+            () => Debug.Log("parar de descansar"));
 
         State goRest = new State("Go Rest",
-            () => Debug.Log("Entrou"),
+            () => Debug.Log("vai descansar"),
             GoRestActions,
-            null);
+            () => Debug.Log("chegou ao descanso"));
 
         State goStage1 = new State("Go Stage1",
-            () => Debug.Log("Entrou"),
+            () => Debug.Log("vai para o palco 1"),
             GoStage1Actions,
-            null);
+            () => Debug.Log("chegou ao palco 1"));
 
         State goStage2 = new State("Go Stage2",
-            () => Debug.Log("Entrou"),
+            () => Debug.Log("vai para o palco 2"),
             GoStage2Actions,
-            null);
+            () => Debug.Log("chegou ao palco 2"));
 
         State watchingStage1 = new State("Watching Stage1",
-            () => Debug.Log("Entrou"),
+            () => Debug.Log("Esta a ver o palco 1"),
             WatchingStage1Actions,
-            null);
+            () => Debug.Log("acabou de ver o palco 1"));
+
+        State watchingStage2 = new State("Watching Stage2",
+            () => Debug.Log("Esta a ver o palco 2"),
+            WatchingStage2Actions,
+            () => Debug.Log("acabou de ver o palco 2"));
 
 
         goEat.AddTransition(
             new Transition(
-                () => _arrived == true,
+                () => _arrive == true,
                 null,
                 eatingState));
 
         goRest.AddTransition(
            new Transition(
-               () => _arrived == true,
+               () => _arrive == true,
                null,
                restingState));
 
@@ -243,19 +293,96 @@ public class NavAgentBehaviour : MonoBehaviour
 
         restingState.AddTransition(
            new Transition(
-               () => (_stamina >= _maxStamina && _health-- <= 0),
+               () => (_stamina >= _maxStamina && _health <= 0),
                null,
                goEat));
 
+        //Go to stages transitions
         eatingState.AddTransition(
            new Transition(
-               () => (_health >= _maxHealth && _stamina > 0), //falta adicionar condição de escolha de palcos
+               () => (_health >= _maxHealth && _stamina > 0 && _stage1Time > _stage2Time),
                null,
                goStage1));
+       
+        eatingState.AddTransition(
+           new Transition(
+               () => (_health >= _maxHealth && _stamina > 0 && _stage2Time > _stage1Time),
+               null,
+               goStage2));
+
+        restingState.AddTransition(
+          new Transition(
+              () => (_stamina >= _maxStamina && _health > 0 && _stage1Time > _stage2Time),
+              null,
+              goStage1));
+
+        restingState.AddTransition(
+           new Transition(
+               () => (_stamina >= _maxStamina && _health > 0 && _stage2Time > _stage1Time),
+               null,
+               goStage2));
+
+        goStage1.AddTransition(
+           new Transition(
+               () => _arrive == true,
+               null,
+               watchingStage1));
+
+        goStage2.AddTransition(
+           new Transition(
+               () => _arrive == true,
+               null,
+               watchingStage2));
+
+        watchingStage1.AddTransition(
+           new Transition(
+               () => _stage1Time <= 0,
+               null,
+               goStage2));
+
+        watchingStage1.AddTransition(
+           new Transition(
+               () => _health <= 0,
+               null,
+               goEat));
+
+        watchingStage1.AddTransition(
+           new Transition(
+               () => _stamina <= 0,
+               null,
+               goRest));
+
+        watchingStage2.AddTransition(
+          new Transition(
+              () => _stage2Time <= 0,
+              null,
+              goStage1));
+
+        watchingStage2.AddTransition(
+           new Transition(
+               () => _health <= 0,
+               null,
+               goEat));
+
+        watchingStage2.AddTransition(
+           new Transition(
+               () => _stamina <= 0 && _health > 0,
+               null,
+               goRest));
 
 
+        //get initial state
+        if(_stage1Time > _stage2Time)
+        {
+            initialState = goStage1;
+        }
+        else 
+        {
+            initialState = goStage2;
+        }
 
-        stateMachine = new StateMachine(goEat);
+
+        stateMachine = new StateMachine(initialState);
     }
 
     private void MoveToFood()
